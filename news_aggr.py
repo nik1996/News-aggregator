4e8ecbcafd877b88252ef6ad27084aa1c55fd46d
Nikhil Jindal <nik.jindal20@gmail.com>
2018-01-15 22:14:42 +0530

    Update news_aggr.py

--- news_aggr.py
+++ news_aggr.py
@@ -1,7 +1,7 @@
 #########################################################################################################
 
 """
-                                                        NEWS AGGREGATOR
+                                                NEWS AGGREGATOR
 """
 
 #########################################################################################################
@@ -22,21 +22,14 @@ from sklearn.metrics import accuracy_score
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
 
 ####Load dataset
 
 filename = "uci-news-aggregator.csv"
 df = pd.read_csv(filename)
 
-#print(df.head())
-#print(df.shape)
-#print(df.groupby('CATEGORY').size())
-
-df = df[['ID', 'TITLE', 'CATEGORY']]
-###df.set_index('ID', inplace=True
+df = df[['TITLE', 'CATEGORY']]
 
 df['TEXT'] = [norm_text(s) for s in df["TITLE"]]
 
@@ -54,15 +47,20 @@ X_train = vectorizer.fit_transform(X_train)
 X_test = vectorizer.transform(X_test)
 
 ####Spot-checking classification algorithms to find best performer
-modeles = []
+models = []
 models.append(('LR',LogisticRegression(class_weight='balanced', multi_class='multinomial', solver='newton-cg')))
 models.append(('DTC',DecisionTreeClassifier()))
-models.append(('KNC',KNeighborsClassifier()))
-models.append(('NB',GaussianNB()))
-models.append(('SVM',SVC()))
+models.append(('NBC',MultinomialNB()))
 
 for name, clf in models:
     kfold = model_selection.KFold(n_splits=10, random_state=7)
     cv_results = model_selection.cross_val_score(clf, X_train, y_train, cv=kfold, scoring='accuracy')
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
+    print(msg)
+
+
+###OUTPUT -: LR: 0.939467 (0.01644)
+###          DTC: 0.906540 (0.002461)
+###          NBC: 0.921235 (0.002376)
+
+### Best performer is Logistic Regression. Parameter tuning is necessary for better performance.
